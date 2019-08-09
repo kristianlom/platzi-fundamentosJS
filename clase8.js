04e06248c8e51308687285b00fd3ed8aa69c4f7a
Kristian Lopez <kristian.lom@gmail.com>
2019-08-09 12:43:07 -0500

    Para metros como referencia o como valor

---
+++ clase8.js
@@ -0,0 +1,41 @@
+var kristian = {
+    nombre: 'Kristian',
+    apellido: 'Lopez',
+    edad: 28
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Susnisky',
+    edad: 27
+}
+
+function imprimirNombreEnMayusculas(persona) {
+    //var nombre = persona.nombre
+    var {nombre} = persona
+
+    console.log(nombre.toUpperCase())
+}
+
+imprimirNombreEnMayusculas(kristian)
+imprimirNombreEnMayusculas(dario)
+//imprimirNombreEnMayusculas({nombre: 'Pepito'})
+//imprimirNombreEnMayusculas({apellido: 'Gomez'})
+
+function imprimirNombreYEdad(persona) {
+    //Reto:
+    //Hola, me llamo Kristian y tengo 28 años
+    var {nombre, apellido, edad} = persona
+
+    console.log(`Hola. me llamo ${nombre} ${apellido} y tengo ${edad} años`)
+}
+
+imprimirNombreYEdad(kristian)
+imprimirNombreYEdad(dario)
+
+function cumpleanos(persona) {
+    return {
+        ...persona,
+        edad: persona.edad + 1
+    }
+}
