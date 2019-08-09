c8230b12cacca5779705455db131505a6a19be2e
Kristian Lopez <kristian.lom@gmail.com>
2019-08-09 11:17:57 -0500

    Creando Funciones

---
+++ clase4.js
@@ -0,0 +1,12 @@
+var nombre = 'Kristian', edad = 28
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Eri', 24)
+imprimirEdad('Dario', 27)
+imprimirEdad(25, 'Carlos')
+imprimirEdad('Juan')
