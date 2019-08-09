18f3b010c3e1194f699f2bdb9d3ec0ccffb7a50c
Kristian Lopez <kristian.lom@gmail.com>
2019-08-09 17:04:09 -0500

    Estructura repetitiva FOR

--- clase.js
+++ clase.js
@@ -2,55 +2,23 @@ var kristian = {
     nombre: 'Kristian',
     apellido: 'Lopez',
     edad: 28,
-    ingeniero: true,
-    cocinero: false,
-    cantante: true,
-    dj: false,
-    guitarrista: false,
-    drone: false
+    peso: 75
 }
-var juan = {
-    nombre: 'Juan',
-    edad: 6
-}
+console.log(`Al inicio del año ${kristian.nombre} pesa ${kristian.peso} kg `)
 
-function imprimirProfesiones(persona) {
-    console.log(`${persona.nombre} es :`)
-    if (persona.ingeniero) {
-        console.log('Ingeniero')
-    } else {
-        console.log('No es Ingeniero')
-    }
-    if (persona.cocinero) {
-        console.log('Cocinero')
-    }
-    if (persona.cantante) {
-        console.log('Cantante')
-    }
-    if (persona.dj) {
-        console.log('DJ')
-    }
-    if (persona.guitarrista) {
-        console.log('Guitarrista')
-    }
-    if (persona.drone) {
-        console.log('Piloto de Drone')
+const DIAS_DEL_ANO = 365
+const INCREMENTO_PESO = 0.200
+
+const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
+const adelgazarDePeso = persona => persona.peso -= INCREMENTO_PESO
+
+for (var i = 1; i <= DIAS_DEL_ANO; i++) {
+    var random = Math.random()
+    if (random < 0.25) {
+        aumentarDePeso(kristian)
+    } else if (random < 0.50) {
+        adelgazarDePeso(kristian)
     }
 }
 
-const MAYORIA_DE_EDAD = 18
-
-const esMayorDeEdad = ({edad}) => edad >= MAYORIA_DE_EDAD
-
-function imprimirSiEsMayorDeEdad(persona) {
-    console.log(`${persona.nombre} es ${esMayorDeEdad(persona) ? "mayor" : "menor"} de edad`)
-}
-
-function permitirAcceso(persona) {
-    if(!esMayorDeEdad(persona)){
-        console.log('ACCESO DENEGADO')
-    }
-}
-
-imprimirSiEsMayorDeEdad(kristian)
-imprimirSiEsMayorDeEdad(juan)
+console.log(`Al final del año ${kristian.nombre} pesa ${kristian.peso.toFixed(1)} kg `)
