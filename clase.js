e33a0082b632904f2325d96bf16bbdd19e285dc4
Kristian Lopez <kristian.lom@gmail.com>
2019-08-10 13:14:54 -0500

    Condicional múltiple SWITCH

--- clase.js
+++ clase.js
@@ -1,24 +1,29 @@
-var kristian = {
-    nombre: 'Kristian',
-    apellido: 'Lopez',
-    edad: 28,
-    peso: 75
+var signo = prompt('Mensaje')
+
+
+switch (signo) {
+    case 'acuario':
+    case 'Acuario':
+        console.log('Switch Acuario');
+        break
+    case 'piscis':
+        console.log('Switch Piscis');
+        break
+    case 'cancer':
+        console.log('Switch Cancer');
+        break
+    case 'capricornio':
+        console.log('Switch Capricornio');
+        break
+    default:
+        console.log('No existe el signo indicado')
 }
-console.log(`Al inicio del año ${kristian.nombre} pesa ${kristian.peso} kg `)
-
-const DIAS_DEL_ANO = 365
-const INCREMENTO_PESO = 0.200
-
-const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
-const adelgazarDePeso = persona => persona.peso -= INCREMENTO_PESO
-
-for (var i = 1; i <= DIAS_DEL_ANO; i++) {
-    var random = Math.random()
-    if (random < 0.25) {
-        aumentarDePeso(kristian)
-    } else if (random < 0.50) {
-        adelgazarDePeso(kristian)
-    }
+if (signo === 'acuario') {
+    console.log('Es Acuario')
 }
-
-console.log(`Al final del año ${kristian.nombre} pesa ${kristian.peso.toFixed(1)} kg `)
+if (signo === 'piscis') {
+    console.log('Es Piscis')
+}
+if (signo === 'cancer') {
+    console.log('Es Cancer')
+}
