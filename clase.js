9edf188c320f4aeb432833da237b03197a82e952
Kristian Lopez <kristian.lom@gmail.com>
2019-08-10 14:01:26 -0500

    Reducir un array a un valor

--- clase.js
+++ clase.js
@@ -1,60 +1,56 @@
 var kristian = {
     nombre: 'Kristian',
     apellido: 'Lopez',
-    altura: 1.72
+    altura: 1.72,
+    cantidadDeLibros: 111
 }
 
 var alan = {
     nombre: 'Alan',
     apellido: 'Perez',
-    altura: 1.86
+    altura: 1.86,
+    cantidadDeLibros: 78
 }
 
 var martin = {
     nombre: 'Martin',
     apellido: 'Gomez',
-    altura: 1.85
+    altura: 1.85,
+    cantidadDeLibros: 132
 }
 
 var dario = {
     nombre: 'Dario',
     apellido: 'Juarez',
-    altura: 1.71
+    altura: 1.71,
+    cantidadDeLibros: 90
 }
 
 var vicky = {
     nombre: 'Vicky',
     apellido: 'Zapata',
-    altura: 1.56
+    altura: 1.56,
+    cantidadDeLibros: 91
 }
 
 var paula = {
     nombre: 'Paula',
     apellido: 'Barros',
-    altura: 1.76
+    altura: 1.76,
+    cantidadDeLibros: 182
 }
 
-const esAlta = ({altura}) => altura > 1.8
+var personas = [kristian, dario, alan, martin, vicky, paula]
 
-var personas = [kristian, alan, martin, dario, vicky, paula]
+//var acum = 0
+// for (var i=0;i<personas.length; i++){
+//
+//     acum += personas[i].cantidadDeLibros
+// }
 
-var personasAltas = personas.filter(esAlta)
+const reducer = (acum, {cantidadDeLibros}) =>
+    acum + cantidadDeLibros
 
-const pasarAlturaACms = (persona) => {
-    return {
-        ...persona,
-        altura: persona.altura * 100
-    }
-}
-const pasarAlturaACms2 = persona => ({
-    ...persona,
-    altura: persona.altura * 100
-})
+var totalDeLibros = personas.reduce(reducer, 0)
 
-var personasCms = personas.map(pasarAlturaACms)
-var personasCms2 = personas.map(pasarAlturaACms2)
-
-console.log(personasAltas)
-console.log(personas)
-console.log(personasCms)
-console.log(personasCms2)
+console.log('Total de libros = ' + totalDeLibros)
