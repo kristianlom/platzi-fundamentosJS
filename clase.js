c0255b6a04b260d4e6152af390ad5688f5080aea
Kristian Lopez <kristian.lom@gmail.com>
2019-08-10 19:43:42 -0500

    Haciendo múltiples request

--- clase.js
+++ clase.js
@@ -1,16 +1,25 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 
-aBuscar = prompt('Numero de algun personaje de Star Wars')
-
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', aBuscar)}`
 const opts = {crossDomain: true}
 
 const onPeopleResponse = function (person) {
-    console.log(`Hola yo soy, ${person.name}`)
+
+    console.log(`${person.url.substr(person.url.length - 3, 2)} , soy ${person.name}`)
+    // console.log(`Hola -${person.id}-, yo soy ${person.name}`)
 }
 
-$.get(lukeUrl, opts, onPeopleResponse)
-    .fail(function () {
-        console.log('El personaje NO EXISTE!!!')
-    })
+function obtenerPersonaje(id) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+
+    $.get(url, opts, onPeopleResponse)
+        .fail(function () {
+            console.log('El personaje NO EXISTE!!!')
+        })
+
+}
+
+for (var i = 1; i < 100; i++) {
+    console.log(i)
+    obtenerPersonaje(i)
+}
