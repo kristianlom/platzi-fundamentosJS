9b19efe2b111a86f95d5b586522a3d3c5d97e0c8
Kristian Lopez <kristian.lom@gmail.com>
2019-08-10 19:31:39 -0500

    Utilizando los callbacks

--- clase.js
+++ clase.js
@@ -1,55 +1,16 @@
-class Persona {
-    constructor(nombre, apellido, altura) {
-        this.nombre = nombre
-        this.apellido = apellido
-        this.altura = altura
-    }
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
 
-    saludar(fn) {
-        var {nombre, apellido} = this
-        console.log(`Hola me llamo ${nombre} ${apellido}`)
-        if (fn) {
-            fn(nombre, apellido, false)
-        }
-    }
+aBuscar = prompt('Numero de algun personaje de Star Wars')
 
-    soyAlto() {
-        var {nombre, altura} = this
-        if (altura > 1.5) {
-            console.log(`${nombre} es una persona Alta`)
-        }
-    }
+const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', aBuscar)}`
+const opts = {crossDomain: true}
+
+const onPeopleResponse = function (person) {
+    console.log(`Hola yo soy, ${person.name}`)
 }
 
-class Desarrollador extends Persona {
-
-    constructor(nombre, apellido, altura) {
-        super(nombre, apellido, altura)
-    }
-
-    saludar(fn) {
-        var {nombre,apellido} = this
-        console.log(`Hola, me llamo ${nombre} ${apellido} y soy desarrollador`)
-        if (fn) {
-            fn(nombre, apellido, true)
-        }
-    }
-
-}
-
-function responderSaludo(nombre, apellido, esDev) {
-    console.log(`Buen día ${nombre} ${apellido}`)
-    if (esDev) {
-        console.log(`Ah mira, no sabía que eras desarrollador(a)`)
-    }
-}
-
-var kristian = new Persona('Kristian', 'Lopez', 1.7)
-var david = new Persona('David', 'Lopez', .9)
-var goliat = new Persona('Goliat', 'Desconocido', 3)
-var desarrollador = new Desarrollador('Kris', 'Lopez Montes', 1.74)
-
-kristian.saludar()
-david.saludar(responderSaludo)
-goliat.saludar(responderSaludo)
-desarrollador.saludar(responderSaludo)
+$.get(lukeUrl, opts, onPeopleResponse)
+    .fail(function () {
+        console.log('El personaje NO EXISTE!!!')
+    })
